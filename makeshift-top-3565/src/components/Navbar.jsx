96b55c273efdb0f0d10234b5ab92716d4a761aa1
dragon1056 <mukunsha@gmail.com>
2023-02-23 23:52:00 +0530

    files added

--- makeshift-top-3565/src/components/Navbar.jsx
+++ makeshift-top-3565/src/components/Navbar.jsx
@@ -2,35 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a href="/" className="nav-link">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="/shop" className="nav-link">
-            Shop
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="/cart" className="nav-link">
-            Cart
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="/login" className="nav-link">
-            Login
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="/signup" className="nav-link">
-            Signup
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>Navbar</div>
   )
 }
 

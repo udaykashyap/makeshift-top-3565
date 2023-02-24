2a4e2ae42977fea9198ee1d2f71fd9fc9f43f2b7
udaykashyap <udaykashyap.hjp@gmail.com>
2023-02-24 15:59:52 +0530

    Checkout Structure

--- makeshift-top-3565/src/App.js
+++ makeshift-top-3565/src/App.js
@@ -1,12 +1,14 @@
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import React from 'react'
+import CheckoutPage from './components/CheckoutPage'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Footer/>
+      {/* <Navbar /> */}
+      <CheckoutPage />
+      <Footer />
     </div>
   )
 }
